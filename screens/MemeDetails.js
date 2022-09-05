776a23b329ec8ef6fb8ffed1854f1789606d1c87
ch-shubham <shubham128166@gmail.com>
2022-09-05 11:59:57 +0530

    Creating MemeDetails Screen Layout

--- screens/MemeDetails.js
+++ screens/MemeDetails.js
@@ -1,7 +1,42 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 
-// TODO: Make it dynamic with real data.
 export default function MemeDetails() {
-  return <Text>Meme Details Screen</Text>;
+  return (
+    <View style={styles.container}>
+      <View style={styles.imageContainer}></View>
+      <View style={styles.actionContainer}>
+        <View style={styles.navigationButton}></View>
+        <View style={styles.actionContainerDivider}></View>
+        <View style={styles.navigationButton}></View>
+      </View>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'red',
+    padding: 20,
+  },
+  imageContainer: {
+    flex: 5,
+    padding: 10,
+    backgroundColor: 'yellow',
+  },
+  actionContainer: {
+    flex: 1,
+    padding: 10,
+    flexDirection: 'row',
+    backgroundColor: 'green',
+  },
+  navigationButton: {
+    flex: 1,
+    backgroundColor: 'pink',
+  },
+  actionContainerDivider: {
+    flex: 3,
+    backgroundColor: 'white',
+  },
+});
